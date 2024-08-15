186a892a963665d037f01129abc408a99c92d415
Jonatã Oliveira <jonata.96ol@gmail.com>
2024-08-15 09:17:33 -0300

    fix: 🐞 ios google auth

--- google-auth.js
+++ google-auth.js
@@ -15,11 +15,8 @@ export default async ({ req, res, log, error }) => {
 		const payload = JSON.parse(req.body)
 
 		// ----------> Get Google access token <----------
-		log(payload)
+
 		const { data } = await axios.get(`https://oauth2.googleapis.com/tokeninfo?id_token=${payload.idToken}`)
-		log(process.env.GOOGLE_CLIENT_ID_ANDROID)
-		log(process.env.GOOGLE_CLIENT_ID_IOS)
-		log(data.aud)
 
 		if (!data) throw new Error('Invalid request.')
 		if (data.aud !== process.env.GOOGLE_CLIENT_ID_ANDROID && data.aud !== process.env.GOOGLE_CLIENT_ID_IOS)
@@ -30,32 +27,19 @@ export default async ({ req, res, log, error }) => {
 		// ----------> Create AppWrite session <----------
 
 		const search = await users.list([Query.equal('email', email)])
-		log(search)
-		let exists = false
 
-		if (Object.keys(search).length > 0) {
-			if (!search.total) return
-			if (!search.users) return
-			if (!search.users[0]) return
-
-			exists = true
-		}
-
-		if (!exists) {
+		if (search.total > 0) {
 			const newUser = await users.create(ID.unique(), email, undefined, undefined, name)
 
 			const token = await users.createToken(newUser.$id)
-			log(token)
 
 			return res.send(token)
 		}
 
 		const token = await users.createToken(search.users[0].$id)
-		log(token)
 
 		return res.send(token)
 	} catch (exception) {
-		log('exception ', exception)
 		error(exception)
 		return res.send('Authentication failed, please try again later.', 500)
 	}
