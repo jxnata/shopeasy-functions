3cf6b09c00a58bec5728843a246f7c9bd6c498f0
jxnata <jxnata@proton.me>
2024-08-22 13:39:08 -0300

    feat: ✨ ai usage and violations

--- ai-suggestions.js
+++ ai-suggestions.js
@@ -1,4 +1,18 @@
+import { Client, Databases, ID, Users } from 'node-appwrite'
 import axios from 'axios'
+
+const client = new Client()
+
+client
+	.setEndpoint(process.env.APPWRITE_ENDPOINT)
+	.setProject(process.env.APPWRITE_PROJECT_ID)
+	.setKey(process.env.APPWRITE_API_KEY)
+
+const users = new Users(client)
+const database = new Databases(client)
+
+const ONE_DAY = 24 * 60 * 60 * 1000
+const DAY_LIMIT = 1000
 
 export default async ({ req, res, log, error }) => {
 	try {
@@ -7,11 +21,30 @@ export default async ({ req, res, log, error }) => {
 		if (!payload.items) throw new Error('Invalid request: items is required')
 		if (!payload.items.length) throw new Error('Empty list items')
 
+		// ----------> Verify AI usage limit <----------
+		const userId = req.headers['x-appwrite-user-id']
+		log(userId)
+
+		if (!userId) throw new Error('Missing user ID')
+
+		const prefs = await users.getPrefs(userId)
+
+		let current = 0
+
+		if (prefs.ai_usage) {
+			current = prefs.ai_usage
+			const last_usage = prefs.last_usage || 0
+
+			if (last_usage < Date.now() - ONE_DAY) current = 0
+
+			if (current > DAY_LIMIT) throw new Error('AI usage limit exceeded')
+		}
+
 		// ----------> Call OpenAI to get suggestions <----------
 
 		const list = payload.items.join(', ')
-		const prompt = `Given the following shopping list: ${list}, suggest 10 other items that might be useful to add to this list. Do not repeat the existing items. Return only the items, separated by commas.`
-		log(prompt)
+		const prompt = `Given the following shopping list: ${list}, suggest 10 other items that might be useful to add to this list. Do not repeat the existing items. Return only the items, separated by commas. If you can't help me, just return the word ERROR.`
+
 		const { data } = await axios.post(
 			'https://api.openai.com/v1/chat/completions',
 			{
@@ -27,7 +60,6 @@ export default async ({ req, res, log, error }) => {
 			}
 		)
 
-		log(data)
 		// ----------> Validate OpenAI response <----------
 
 		if (!data) throw new Error('Invalid response')
@@ -35,6 +67,25 @@ export default async ({ req, res, log, error }) => {
 		if (!data.choices.length) throw new Error('Choices is empty')
 		if (!data.choices[0].message) throw new Error('No message in choice')
 		if (!data.choices[0].message.content) throw new Error('No content in message')
+
+		// ----------> Save user preferences <----------
+
+		const usage = data.usage.total_tokens
+		users.updatePrefs(userId, { ai_usage: current + usage, last_usage: Date.now() })
+
+		// ----------> Check violations <----------
+
+		if (data.choices[0].message.content.includes('ERROR')) {
+			log('---> violation <---')
+			const violation = await database.createDocument('production', 'violation', ID.unique(), {
+				user: userId,
+				content: list,
+			})
+			log(violation)
+			throw new Error('Explicit content')
+		}
+
+		// ----------> Return suggestions <----------
 
 		const suggestionsString = data.choices[0].message.content
 
@@ -45,7 +96,7 @@ export default async ({ req, res, log, error }) => {
 
 		return res.send(suggestions)
 	} catch (exception) {
-		error(exception)
+		error(exception.toString())
 		return res.send('Suggestion failed, please try again later.', 500)
 	}
 }
